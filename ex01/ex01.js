0bc9c940cc2581f725a718fafcc5f640672d2e8a
adrien <adrien.charrier@outlook.fr>
2021-11-29 10:28:16 +0100

    ADD - Ev01

---
+++ ex01/ex01.js
@@ -0,0 +1,12 @@
+/*
+* Reltun true if the string is a palindrome
+*/
+function isPalindrome(string){
+    for (let i = 0 ; i < string.length ; i++) {
+        if(string[i] !== string[string.length - 1 - i]) 
+            return false;
+    }
+    return true;
+}
+    
+exports.isPalindrome =  isPalindrome;
