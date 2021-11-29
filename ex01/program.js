602cc2e58bfdbb13edc1936b7f6b21c4b687b2c5
adrien <adrien.charrier@outlook.fr>
2021-11-29 14:05:40 +0100

    optimisation ex01

--- ex01/program.js
+++ ex01/program.js
@@ -2,7 +2,7 @@
 * Reltun true if the string is a palindrome
 */
 function isPalindrome(string){
-    for (let i = 0 ; i < string.length ; i++) {
+    for (let i = 0 ; i < Math.ceil(string.length/ 2) ; i++) {
         if(string[i] !== string[string.length - 1 - i]) 
             return false;
     }
