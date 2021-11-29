b93a5e9cb1628e0ea7f8a1dbb62ec391394d904c
adrien <adrien.charrier@outlook.fr>
2021-11-29 11:42:43 +0100

    ADD - Ex03

---
+++ ex03/program.js
@@ -0,0 +1,29 @@
+/*
+* Return the index of the first none repeted character in the string.
+*/
+
+
+function firstNonRepeatingCharacter(string) {
+    const characters = {};
+
+    for (let i = 0; i < string.length; i++) {
+        if (characters[string[i]] === undefined) {
+            characters[string[i]] = 1;
+        } else {
+            characters[string[i]] = characters[string[i]] + 1;
+        }
+    }
+    
+    for (let i = 0; i < string.length; i++) {
+        if (characters[string[i]] === 1) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(firstNonRepeatingCharacter("abcadbdcff"))
+  
+    
+// Do not edit the line below.
+exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
